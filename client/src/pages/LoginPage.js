6412462d166abc5c65644db64719e1c1cb44e479
CruiseDevice <meakashchavan@gmail.com>
2024-07-07 02:01:18 -0700

    login and register page done

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,12 @@
 export default function LoginPage () {
   return (
-    <div>
-      Login page
+    <div class="auth-container login-container">
+      <h2>Login</h2>
+      <from>
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button>Login</button>
+      </from>
     </div>    
   );
 }
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,5 +1,12 @@
 export default function RegisterPage () {
   return (
-    <div>Register</div>
+    <div class="auth-container register-container">
+      <h2>Register</h2>
+      <form>
+        <input type="text" placeholder="username"/>
+        <input type="password" placeholder="password"/>
+        <button>Register</button>
+      </form>
+    </div>
   );
 }
