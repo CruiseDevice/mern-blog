3a6ce89db5f66e41a0577d9683287232f2f70876
CruiseDevice <meakashchavan@gmail.com>
2024-07-08 00:50:17 -0700

    Add loging register logout
    - hide login register button in header when user is logged in

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ const { default: mongoose } = require('mongoose');
 const User = require('./models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
 app.use(express.json());
+app.use(cookieParser());
 
 const SALTROUNDS = 10;
+const secret = 'jwt_secret';
 
 mongoose.connect('mongodb+srv://root:saturn12345@cluster0.clv9ruy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
 
@@ -54,7 +57,7 @@ app.post('/login', async (req, res) => {
     }
 
     // Generate JWT
-    const token = jwt.sign({id: user._id}, 'jwt_secret', {expiresIn: '1h'});
+    const token = jwt.sign({id: user._id}, secret, {expiresIn: '1h'});
     res.cookie('token', token).json('ok');
   } catch (e) {
     console.log(e);
@@ -64,5 +67,18 @@ app.post('/login', async (req, res) => {
   }
 });
 
+app.post('/logout', (req, res) => {
+  res.cookie('token', '').json('ok');
+});
+
+app.get('/profile', (req, res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) {
+      throw err;
+    }
+    res.json(info);
+  })
+});
 
 app.listen(4000);
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,13 +1,44 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header () {
-    return (
-      <header>
-        <Link to="/">MyBlog</Link>
-        <nav>
+  const [userId, setUserId] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(user => {
+        setUserId(user.id);
+      });
+    });
+  }, []);
+
+  function logout () {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST'
+    });
+    setUserId(null);
+  }
+
+  return (
+    <header>
+      <Link to="/">MyBlog</Link>
+      <nav>
+        {userId && (
+          <>
+            <Link to="/create">Create New Post</Link>
+            <Link onClick={logout}>Logout</Link>
+          </>
+        )}
+        {!userId && (
+          <>
           <Link to="/login">Login</Link>
           <Link to="/register">Register</Link>
-        </nav>
-      </header>
-    );
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
